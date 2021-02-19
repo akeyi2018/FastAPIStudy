9399803252218338b18a562f11f746d9da9d13a1
akeyi2018 <akeyi2016@gmail.com>
2021-02-20 04:15:35 +0900

    add http response

--- main.py
+++ main.py
@@ -1,14 +1,45 @@
 import uvicorn
 from fastapi import Depends, FastAPI
 from fastapi.security import OAuth2PasswordBearer
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-@app.get("/")
+def generate_html_response():
+    html_content = """
+<html>
+  <head>
+    <title>Some HTML in here</title>
+  </head>
+  <body>
+    <h1>Look ma! HTML!</h1>
+    <h2>test</h2>
+ 
+    <a href="https://www.yahoo.co.jp">リンク先にジャンプ</a>	
+    
+  </body>
+</html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
+
+
+@app.get("/", response_class=HTMLResponse)
 async def root():
-    return {"message": "Hello World"}
+    res = """
+    <head>	 
+<title>文書番号002</title>	 
+<link rel="index" href="https://www.yahoo.co.jp">	
+<link rel="contents" href="mokuji.htm">	
+<link rel="search" href="kensaku.html">	
+<link rel="help" href="help.html">	
+<link rel="prev" href="001.htm">	
+<link rel="next" href="003.htm">	
+</head>
+    """
+    # return {"message": "Hello World"}
+    return generate_html_response()
 
 @app.get("/items/")
 async def read_items(token: str = Depends(oauth2_scheme)):
